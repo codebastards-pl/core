dd84bf23c07e815319e465234f7c9d339d959c76
m-codebastards <m@codebastards.pl>
2015-01-20 14:09:54 +0100

    update mailers

--- app/mailers/shoppe/order_mailer.rb
+++ app/mailers/shoppe/order_mailer.rb
@@ -1,25 +1,25 @@
 module Shoppe
   class OrderMailer < ActionMailer::Base
-  
+
     def received(order)
       @order = order
-      mail :from => Shoppe.settings.outbound_email_address, :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.subject', :default => "Order Confirmation")
+      mail :from => Shoppe.settings.outbound_email_address, :to => order.email_address, :subject => 'Potwierdzenie złożenia zamówienia w Włóczka Cafe'
     end
-  
+
     def accepted(order)
       @order = order
-      mail :from => Shoppe.settings.outbound_email_address, :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.accepted', :default => "Order Accepted")
+      mail :from => Shoppe.settings.outbound_email_address, :to => order.email_address, :subject => 'Zamówienie przyjęte do realizacji'
     end
-  
+
     def rejected(order)
       @order = order
       mail :from => Shoppe.settings.outbound_email_address, :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.rejected', :default => "Order Rejected")
     end
-  
+
     def shipped(order)
       @order = order
-      mail :from => Shoppe.settings.outbound_email_address, :to => order.email_address, :subject => I18n.t('shoppe.order_mailer.received.shipped', :default => "Order Shipped")
+      mail :from => Shoppe.settings.outbound_email_address, :to => order.email_address, :subject => 'Zamówienie zostało wysłane'
     end
-    
+
   end
 end
